0d4372f0431bb95eab50142d92ce0fce7ce630b8
glucciani <giancarlolucciani@gmail.com>
2020-06-28 18:14:55 -0300

    Commit inicial dos arquivos do projeto.

---
+++ src/test/java/com/lucciani/news/api/NewsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucciani.news.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
